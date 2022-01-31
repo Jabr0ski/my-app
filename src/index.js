103fa144c425612243213e1c72c7538fb9ffb1ee
b1ggl3s <98510771+b1ggl3s@users.noreply.github.com>
2022-02-01 00:11:12 +0800

    Add files via upload
    Writes game over.

--- src/index.js
+++ src/index.js
@@ -2,66 +2,99 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props){
+
+
+function Square(props) {
+
   return (
     <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
-  );
+  )
 }
 
-function coordinatesCalc(i){
-  if(i % 3 === 0 ){
-    return ('a' + (Math.floor(i/3)+1))
-  } else if (i % 3 === 1){
-    return ('b' + (Math.floor(i/3)+1))
+function getCoordinates(i) {
+  let column = ''
+  if (i % 3 === 0) {
+    column = 'a'
+  } else if (i % 3 === 1) {
+    column = 'b';
   } else {
-    return ('c' + (Math.floor(i/3)+1))
-  }
+    column = 'c';
+  };
+
+  let row = Math.floor(i / 3) + 1
+
+  let coordinate = column + row
+
+  return (
+    coordinate
+  )
 }
+
+
+
 
 class Board extends React.Component {
-  
-  renderSquare(i) {
-    return (
-      <Square 
-        value={this.props.squares[i]} 
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
 
-  renderCoordinate(t, c){
-    return (
-      <div className = {t}>
-        {c}
+  renderColumnCoordinate(i) {
+    return(
+      <div className="columncoordinate">
+        {i}
       </div>
     )
   }
 
+  renderRowCoordinate(i) {
+    return(
+      <div className="rowcoordinate">
+        {i}
+      </div>
+    )
+  }
+
+  renderEmptySquare() {
+    return(
+      <div className="emptysquare">
+      </div>
+    )
+  }
+
+  renderSquare(i) {
+    document.documentElement.style.setProperty('--custom-height', '4px');
+    document.documentElement.style.setProperty('--custom-width', '4px');
+    return (
+    <Square 
+    value={this.props.squares[i]} 
+    onClick={() => this.props.onClick(i)}
+    />
+    );
+  }
+
   render() {
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderCoordinate('cornerCoordinate', null)}
-          {this.renderCoordinate('rowCoordinate', 'A')}
-          {this.renderCoordinate('rowCoordinate', 'B')}
-          {this.renderCoordinate('rowCoordinate', 'C')}
+        <div>
+          {this.renderEmptySquare()}
+          {this.renderColumnCoordinate('a')}
+          {this.renderColumnCoordinate('b')}
+          {this.renderColumnCoordinate('c')}
         </div>
         <div className="board-row">
-          {this.renderCoordinate('columnCoordinate', '1')}
+          {this.renderRowCoordinate('1')}
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderCoordinate('columnCoordinate', '2')}
+          {this.renderRowCoordinate('2')}
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderCoordinate('columnCoordinate', '3')}
+          {this.renderRowCoordinate('3')}
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
@@ -72,37 +105,42 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       history: [{
         squares: Array(9).fill(null),
       }],
-      coordinates: [null],
       stepNumber: 0,
       xIsNext: true,
+      coordinates: [null],
+      winners: ['']
     }
   }
 
-  handleClick(i){
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+  handleClick(i) {
+    const history = this.state.history.slice(0,
+      this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    const coordinates = this.state.coordinates.slice(0, this.state.stepNumber + 1);
-    if(calculateWinner(squares) || squares[i]){
+    const coordinates = this.state.coordinates.slice(0, 
+      this.state.stepNumber + 1)
+    const coordinate = getCoordinates(i)
+    const winners = this.state.winners.slice(0, this.state.stepNumber + 1)
+    
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
-    const coordinate = coordinatesCalc(i);
+    const winner = calculateWinner(squares) ? ' (Game Over)' : ''
+
     this.setState({
-      history: history.concat([{
-        squares: squares,
+      history: history.concat([{squares: squares,
       }]),
-      coordinates: coordinates.concat([
-        coordinate
-      ]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+      coordinates: coordinates.concat([coordinate]),
+      winners: winners.concat([winner])
     });
   }
 
@@ -113,42 +151,41 @@ class Game extends React.Component {
     });
   }
 
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+    const coordinates = this.state.coordinates;
+    const winners = this.state.winners
 
     const moves = history.map((step, move) => {
       const desc = move ?
-      'Go to move #' + move + " " + this.state.coordinates[move]:
-      'Go to game start';
+        'Go to move #' + move + ' ' + coordinates[move] + winners[move]:
+        'Go to game start';
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
         </li>
-      )
+      );
     });
+
     let status;
     if (winner) {
-      status = 'Winner ' + winner;
+      status = 'Winner: ' + winner;
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' :
+      'O');
     }
-
     return (
       <div className="game">
         <div className="game-board">
           <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+          squares={current.squares}
+          onClick={(i) => this.handleClick(i)}
           />
-          <ol>
-            {
-              this.state.coordinates.slice(1).map((i) => (
-                  <li key={i}>{this.state.coordinates.indexOf(i) % 2 ? 'X' : 'O'} in {i}</li>
-              ))
-            }
-          </ol>
         </div>
         <div className="game-info">
           <div>{status}</div>
@@ -158,6 +195,14 @@ class Game extends React.Component {
     );
   }
 }
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
 
 function calculateWinner(squares) {
   const lines = [
@@ -170,18 +215,12 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+  for(let i = 0; i < lines.length; i++) {
+    const[a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] ===
+    squares[c]) {
       return squares[a];
     }
   }
   return null;
 }
-
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
